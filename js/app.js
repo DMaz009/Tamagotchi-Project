6388c22ec60118fac82e0dc6e7ec3cb780948c84
David Mazza <DavidMazza@Davids-MacBook-Pro.local>
2021-12-06 12:23:20 -0600

    Second Commit Styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 console.log("Tomagotchi");
+
+class Pets {
+  constructor() {
+    this.hunger =
+    this.sleep =
+    this.boredom =
+    this.age =
+  }
+}
+
+
+
+
+class Game {
+  constructor () {
+
+  }
+}
+
+
+
+
+const app = {
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+//
